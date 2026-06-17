a24e348f3e6637ca05fe6653fe038f4883284d8e
Nitanshu Tak <152681890+Nitanshu715@users.noreply.github.com>
2026-06-17 06:43:30 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -19,8 +19,8 @@ export default defineConfig({
         scope: '/',
         start_url: '/',
         icons: [
-          { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png' }
+          { src: '/Sal-Shield-Logo.png', sizes: '192x192', type: 'image/png' },
+          { src: '/Sal-Shield-Logo.png', sizes: '512x512', type: 'image/png' }
         ]
       },
       workbox: {
